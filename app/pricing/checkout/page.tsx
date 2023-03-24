faa70dd1786a84b5be438a2c92d02a75f940a8a7
MassyWeeWee <wyattconnollyfitness@gmail.com>
2023-03-24 11:20:00 -0700

    react form hook on checkout page with styles

--- app/pricing/checkout/page.tsx
+++ app/pricing/checkout/page.tsx
@@ -1,6 +1,23 @@
-import Image from "next/image";
+"use client";
 
+import Image from "next/image";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface IFormInput {
+  firstName: string;
+  lastName: string;
+  zipCode: any;
+  phoneNumber: number;
+  email: string;
+}
 function Page() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
   return (
     <div className="lg:h-[calc(100vh_-_136px)] mx-auto max-w-screen-xl px-4 py-8 flex justify-center items-center">
       <div className=" w-full xl:w-3/4 lg:w-11/12 flex justify-center gap-16">
@@ -15,7 +32,10 @@ function Page() {
 
         <div className="w-full lg:w-7/12 bg-white  rounded-lg lg:rounded-l-none">
           <h3 className="text-2xl text-center">Review & Confirm Membership</h3>
-          <form className="bg-white rounded pt-6">
+          <form
+            className="bg-white rounded pt-6"
+            onSubmit={handleSubmit(onSubmit)}
+          >
             <div className="flex flex-col sm:flex-row sm:space-x-4 lg:space-x-0 lg:flex-col  ">
               <div className="w-full ">
                 <label
@@ -25,11 +45,19 @@ function Page() {
                   First Name
                 </label>
                 <input
-                  className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                  id="firstName"
+                  className={
+                    `w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none ` +
+                    `${errors.firstName && "border-red-500  "}`
+                  }
                   type="text"
                   placeholder="First Name"
+                  {...register("firstName", { required: true, maxLength: 20 })}
                 />
+                {errors.firstName && (
+                  <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                    First name is required
+                  </span>
+                )}
               </div>
               <div className="w-full mt-2 sm:mt-0 lg:mt-2">
                 <label
@@ -39,10 +67,22 @@ function Page() {
                   Last Name
                 </label>
                 <input
-                  className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  className={
+                    `w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none ` +
+                    `${errors.lastName && "border-red-500  "}`
+                  }
                   type="text"
                   placeholder="Last Name"
+                  {...register("lastName", {
+                    required: true,
+                    pattern: /^[A-Za-z]+$/i,
+                  })}
                 />
+                {errors.lastName && (
+                  <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                    Last name is required
+                  </span>
+                )}
               </div>
             </div>
             <div className=" flex flex-col sm:flex-row sm:space-x-4 justify-center  ">
@@ -54,10 +94,19 @@ function Page() {
                   Zip Code
                 </label>
                 <input
-                  className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  className={
+                    `w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none ` +
+                    `${errors.zipCode && "border-red-500  "}`
+                  }
                   type="text"
                   placeholder="Zip Code"
+                  {...register("zipCode", { required: true, maxLength: 5 })}
                 />
+                {errors.zipCode && (
+                  <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                    Zip Code is required
+                  </span>
+                )}
               </div>
               <div className="w-full sm:w-3/5 mt-2 ">
                 <label
@@ -67,10 +116,19 @@ function Page() {
                   Phone Number
                 </label>
                 <input
-                  className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  className={
+                    `w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none ` +
+                    `${errors.phoneNumber && "border-red-500  "}`
+                  }
                   type="tel"
                   placeholder="Phone Number"
+                  {...register("phoneNumber", { required: true })}
                 />
+                {errors.phoneNumber && (
+                  <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                    Phone number is required
+                  </span>
+                )}
               </div>
             </div>
             <div className="mt-2">
@@ -81,17 +139,26 @@ function Page() {
                 Email
               </label>
               <input
-                className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                className={
+                  `w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none ` +
+                  `${errors.email && "border-red-500"}`
+                }
                 id="email"
                 type="email"
                 placeholder="Email"
+                {...register("email", { required: true })}
               />
+              {errors.email && (
+                <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                  Email address is required
+                </span>
+              )}
             </div>
 
             <div className="my-6 text-center">
               <button
                 className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-                type="button"
+                type="submit"
               >
                 Checkout
               </button>
